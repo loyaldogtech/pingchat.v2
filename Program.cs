860c9f236e822fc0f3f48329b5d2400b70043b6a
Shad Spreiter <shadspreiter@loyaldogtech.onmicrosoft.com>
2026-05-02 19:44:26 -0700

    Configure SignalR services and hub route

--- Program.cs
+++ Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,5 +39,6 @@ app.UseAuthorization();
 app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
+app.MapHub<PingChat.Hubs.ChatHub>("/chatHub");
 
 app.Run();
