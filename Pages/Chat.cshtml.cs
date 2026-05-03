5baaaeccc972925c65bdf324f80069032715948b
Shad Spreiter <shadspreiter@loyaldogtech.onmicrosoft.com>
2026-05-02 17:24:28 -0700

    Protect Chat page with authroization

--- Pages/Chat.cshtml.cs
+++ Pages/Chat.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PingChat.Pages;
 
+[Authorize]
 public class ChatModel : PageModel
 {
     public void OnGet()
