57ec602fa4401b473fcc6e173f27321b24cf4556
Shad Spreiter <shadspreiter@loyaldogtech.onmicrosoft.com>
2026-05-04 21:37:33 -0700

    Add channels navigation and chat page route

--- Pages/Chat.cshtml.cs
+++ Pages/Chat.cshtml.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PingChat.Pages;
@@ -6,6 +7,26 @@ namespace PingChat.Pages;
 [Authorize]
 public class ChatModel : PageModel
 {
+    [BindProperty(SupportsGet = true)]
+    public string Name { get; set; } = string.Empty;
+
+    public string ChannelName => string.IsNullOrWhiteSpace(Name) ? "unknown" : Name;
+
+    public string ChannelType
+    {
+        get
+        {
+            return ChannelName.ToLower() switch
+            {
+                "general" => "Agency + Client",
+                "internal" => "Agency Only",
+                "client-acme" => "Client Channel",
+                "project-redesign" => "Project Room",
+                _ => "Custom Channel"
+            };
+        }
+    }
+
     public void OnGet()
     {
     }
