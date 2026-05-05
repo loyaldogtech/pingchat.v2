1dabec01659df6998af6fdccd26ceb576033eb4a
Shad Spreiter <shadspreiter@loyaldogtech.onmicrosoft.com>
2026-05-04 22:07:12 -0700

    Move chat page messages into the Chat PageModel

--- Pages/Chat.cshtml.cs
+++ Pages/Chat.cshtml.cs
@@ -27,7 +27,17 @@ public class ChatModel : PageModel
         }
     }
 
+    public List<ChatMessageItem> Messages { get; private set; } = new();
+
     public void OnGet()
     {
+        Messages = new List<ChatMessageItem>
+        {
+            new("Amanda", "9:02 AM", $"Welcome to #{ChannelName}."),
+            new("Chris", "9:04 AM", "This is where our real-time SignalR chat will appear next."),
+            new("You", "9:05 AM", "Static page first. Real-time messaging after that.")
+        };
     }
+
+    public record ChatMessageItem(string Author, string Time, string Text);
 }
